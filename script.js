1536f9453ad475708a4c6b4bc1dfadbc6111a81e
ilhamabi <ilhamabirizal365@gmail.com>
2026-09-08 15:03:27 +0700

    create logic for computer choice
    using math.random() will display rock, paper, scissors depend on value

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello World')
+function getComputerChoice() {
+  let number = Math.random();
+
+  if (number <= 0.1) {
+    console.log("Rock");
+  } else if (number <= 0.5) {
+    console.log("Paper");
+  } else {
+    console.log("Scissors");
+  }
+}
